2f6b9ed523d640d25d7c9ba6f500e8fd2f13546c
Veljko28 <charlibear28@gmail.com>
2023-09-28 22:38:55 +0200

    adding prompt creation and feed

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,8 +1,51 @@
-import React from 'react'
+'use client';
+
+import { useState, useEffect, ChangeEvent } from 'react';
+import PromptCard from './PromptCard';
+
+const PromptCardList = ({data, handleTagClick}: {data: any, handleTagClick: any}) => {
+  return (
+    <div className='mt-16 prompt_layout'>
+      {data.map((post: Post) => 
+        <PromptCard key={post._id} 
+        post={post} 
+        handleTagClick={handleTagClick}/>
+      )}
+    </div>
+  )
+}
+
+
 
 const Feed = () => {
+  const [searchText, setSearchText] = useState('')
+  const [posts, setPosts] = useState<Post[]>([])
+
+  const handleSearchChange = (e: ChangeEvent) => {
+
+  } 
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await fetch('/api/prompt');
+      const data = await res.json();
+
+      setPosts(data);
+    }
+
+
+    fetchPosts();
+  }, [])
+
   return (
-    <div>Feed</div>
+    <section className='feed'>
+      <form className='relative w-full flex-center'>
+        <input type="text" placeholder='Search for a tag or username'
+        value={searchText} onChange={(e) => handleSearchChange(e)} required
+        className="search_input peer"/>
+      </form>
+      <PromptCardList data={posts} handleTagClick={() => {}} />
+    </section>
   )
 }
 
