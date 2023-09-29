8f19a112cc154462e36b3a46a58956cc770b0832
Veljko28 <charlibear28@gmail.com>
2023-09-29 21:24:26 +0200

    adding full profile page

--- app/api/prompt/[id]/route.ts
+++ app/api/prompt/[id]/route.ts
@@ -22,7 +22,52 @@ export const GET = async (req: Request, {params}: any) => {
 }
 
 
-export const PATCH = async () => {
+export const PATCH = async (req: Request, { params }: any) => {
+    const { prompt, tag } = await req.json();
     
+    try {
+        await connectToDB();
+
+        const existingPrompt = await Prompt.findById(params.id);
+
+        if (!existingPrompt) return new Response("Prompt not found",
+        {status: 404})
+
+        existingPrompt.prompt = prompt;
+        existingPrompt.tag = tag;
+
+        await existingPrompt.save();
+
+        return new Response(JSON.stringify(existingPrompt), {
+            status: 200
+        });
+    }
+    catch (err){
+        console.error(err);
+
+        return new Response("Failed to update prompt", {
+            status: 500
+        })
+    }
 }
 
+export const DELETE = async (req: Request, {params}: any) => {
+    try {
+        await connectToDB();
+
+        await Prompt.findByIdAndRemove(params.id);
+
+
+        return new Response("Prompt deleted successfully", 
+        {
+            status: 200
+        })
+    }
+    catch (err) {
+        console.error(err);
+
+        return new Response("Failed to delete prompt", {
+            status: 500
+        })
+    }
+}
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -8,10 +8,12 @@ import Profile from '@components/Profile';
 
 const ProfilePage = () => {
 
-    useEffect(() => {
-       const {data: session} = useSession();
-       const [posts, setPosts] = useState([]);
+    const router = useRouter();
+    const {data: session} = useSession();
+    const [posts, setPosts] = useState([]);
 
+    useEffect(() => {
+   
         const fetchPosts = async () => {
           const res = await fetch(`/api/users/${(session as any).user.id}/posts`);
           const data = await res.json();
@@ -23,8 +25,28 @@ const ProfilePage = () => {
         fetchPosts();
       }, [])
 
-    const handleEdit = () => {}
-    const handleDelete = async () => {}
+    const handleEdit = (post: Post) => {
+      router.push(`update-prompt?id=${post._id}`)
+
+    }
+    const handleDelete = async (post: Post) => {
+      const hasConfirmed = confirm("Are you sure you want to delete this prompt?");
+
+      if (hasConfirmed){
+        try {
+            await fetch(`/api/prompt/${post._id?.toString()}`),
+            {
+              method: "DELETE"
+            }
+
+            const filteredPosts = posts.filter((p: Post) => p._id !== post._id);
+            setPosts(filteredPosts);
+          }
+        catch (err){
+          console.error(err);
+        }
+      }
+    }
 
   return (
     <Profile
