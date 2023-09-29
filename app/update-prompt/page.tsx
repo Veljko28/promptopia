9932a5680666d69092555251ac6c916466a41619
Veljko28 <charlibear28@gmail.com>
2023-09-29 21:22:50 +0200

    adding update prompt

---
+++ app/update-prompt/page.tsx
@@ -0,0 +1,74 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
+
+import Form from '@components/Form';
+
+
+const EditPrompt = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const promptId = searchParams.get('id');
+  
+
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: '',
+    tag: ''
+  });
+
+
+  useEffect(() => {
+    const getPromptDetails = async () => {
+        const response = await fetch(`/api/prompt/${promptId}`)
+
+        const data = await response.json();
+
+        setPost({
+            prompt: data.prompt,
+            tag: data.tag
+        })
+    }
+  }, [promptId])
+
+
+  const updatePrompt = async (e: Event) => {
+    e.preventDefault();
+    setSubmitting(true);
+
+    if (!promptId) return alert("Prompt Id not found");
+
+    try {
+      const res = await fetch(`/api/prompt/${promptId}`, {
+        method: "PATCH",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          tag: post.tag
+        })
+      })
+
+      if (res.ok) router.push('/');
+
+
+    }
+    catch (err){
+      console.error(err);
+    }
+    finally {
+      setSubmitting(false);
+    }
+  }
+
+  return (
+    <Form 
+      type="Edit"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={updatePrompt}
+    />
+  )
+}
+
+export default EditPrompt
